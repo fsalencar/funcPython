a45ad0447bf7cd96152fda445747df976bc33f61
Felipe Alencar <fsalencar@outlook.com>
2021-01-10 06:52:32 -0800

    commitando novos dois projects

--- gestao_clientes/urls.py
+++ gestao_clientes/urls.py
@@ -18,8 +18,15 @@ from django.urls import path, include
 from clientes import urls as clientes_urls
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LogoutView
+from home import urls as home_urls
+
 
 urlpatterns = [
+    path('', include(home_urls)),
     path('clientes/', include(clientes_urls)),
+    path('login/', LoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
     path('admin/', admin.site.urls),
 ] +static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
